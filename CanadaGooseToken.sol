783110fed25f4ef3e35a078135f4c31a8d51d5b1
Gerald Wu <gwu02251250@gmail.com>
2017-12-27 17:18:37 -0500

    ICO crowdsale functionality

--- CanadaGooseToken.sol
+++ CanadaGooseToken.sol
@@ -1,21 +1,46 @@
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.19;
 
 import './IERC20.sol';
+import './SafeMath.sol';
 
 contract CanadaGooseToken is IERC20 {
 
-	uint public _totalSupply = 1000000;
+	using SafeMath for uint256;
+
+	uint public _totalSupply = 1000000000000000000000000;
 
 	string public constant symbol = "CGT";
 	string public constant name = "Canada Goose Token";
-	uint8 public constant decimals = 0;
+	uint8 public constant decimals = 18;
+
+	uint256 public constant RATE = 1000000;
+
+	address public owner;
 
 	mapping(address => uint256) balances;
 	mapping(address => mapping(address => uint256)) allowed;
 
+	function () payable
+	{
+		createTokens();
+	}
+
 	function CanadaGooseToken()
 	{
+		owner = msg.sender;
 		balances[msg.sender] = _totalSupply;
+	}
+
+	function createTokens() payable
+	{
+		if(msg.value > 0)
+		{
+			uint256 tokens = msg.value.mul(RATE);
+			balances[msg.sender] = balances[msg.sender].add(tokens);
+			_totalSupply = _totalSupply.add(tokens);
+
+			owner.transfer(msg.value);
+		}
 	}
 
 	function totalSupply() constant returns (uint256 totalSupply)
@@ -32,8 +57,8 @@ contract CanadaGooseToken is IERC20 {
 	{
 		if(balances[msg.sender] >= _value && balances[_to] + _value > balances[_to])
 		{
-			balances[msg.sender] -= _value;
-			balances[_to] += _value;
+			balances[msg.sender] = balances[msg.sender].sub(_value);
+			balances[_to] = balances[_to].add(_value);
 			Transfer(msg.sender, _to, _value);
 			return true;
 		}
@@ -47,9 +72,9 @@ contract CanadaGooseToken is IERC20 {
 	{
 		if(balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to])
 		{
-			balances[_to] += _value;
-			balances[_from] -= _value;
-			allowed[_from][msg.sender] -= _value;
+			balances[_to] = balances[_to].add(_value);
+			balances[_from] = balances[_from].sub(_value);
+			allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
 			Transfer(_from, _to, _value);
 			return true;
 		}
